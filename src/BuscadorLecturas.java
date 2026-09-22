0bbf635d5ec6d9eb3e42f462a13291ae412987cd
julianandreshg28-debug <julianandreshg.28@gmail.com>
2026-09-21 21:17:13 -0500

    feat: implementar busqueda binaria y busqueda por estacion

--- src/BuscadorLecturas.java
+++ src/BuscadorLecturas.java
@@ -39,4 +39,105 @@ public class BuscadorLecturas {
 
         return -1;
     }
+    /**
+     * Busca un timestamp mediante búsqueda binaria.
+     *
+     * PRECONDICIÓN:
+     * las lecturas deben estar ordenadas
+     * ascendentemente por timestamp.
+     *
+     * @return posición de la lectura o -1 si no existe
+     */
+    public static int busquedaBinariaPorTimestamp(
+            LecturaSensor[] datos,
+            String timestamp) {
+
+        comparaciones = 0;
+
+        int inicio = 0;
+        int fin = datos.length - 1;
+
+        while (inicio <= fin) {
+
+            int medio = (inicio + fin) / 2;
+
+            comparaciones++;
+
+            int comparacion =
+                    datos[medio]
+                            .getTimestamp()
+                            .compareTo(timestamp);
+
+            if (comparacion == 0) {
+                return medio;
+            }
+
+            if (comparacion < 0) {
+                inicio = medio + 1;
+            } else {
+                fin = medio - 1;
+            }
+        }
+
+        return -1;
+    }
+    /**
+     * Busca la primera lectura de una estación.
+     *
+     * @return posición de la primera coincidencia o -1
+     */
+    public static int buscarPorEstacion(
+            LecturaSensor[] datos,
+            String idSensor) {
+
+        comparaciones = 0;
+
+        for (int i = 0; i < datos.length; i++) {
+
+            comparaciones++;
+
+            if (datos[i].getIdSensor().equals(idSensor)) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+    /**
+     * Búsqueda binaria por PM2.5.
+     *
+     * PRECONDICIÓN:
+     * el arreglo debe estar ordenado ascendentemente
+     * por PM2.5.
+     *
+     * El generador de la Semana 3 no garantiza esta condición.
+     */
+    public static int busquedaBinariaPorPm25(
+            LecturaSensor[] datos,
+            double pm25) {
+
+        comparaciones = 0;
+
+        int inicio = 0;
+        int fin = datos.length - 1;
+
+        while (inicio <= fin) {
+
+            int medio = (inicio + fin) / 2;
+
+            comparaciones++;
+
+            if (datos[medio].getPm25() == pm25) {
+                return medio;
+            }
+
+            if (datos[medio].getPm25() < pm25) {
+                inicio = medio + 1;
+            } else {
+                fin = medio - 1;
+            }
+        }
+
+        return -1;
+    }
 }
